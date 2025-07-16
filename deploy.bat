078cf8cc49c62baf1e90f1f9c711c5b97906729c
mosinDataBase <msitservices15@gmail.com>
2025-07-17 01:31:16 +0530

    logout added

--- deploy.bat
+++ deploy.bat
@@ -4,7 +4,7 @@ git init
 git add .
 git commit -m "logout added"
 git branch -M main
-git remote add origin https://github.com/mosinDataBase/myalgoserver.git
+REM git remote add origin https://github.com/mosinDataBase/myalgoserver.git
 git push -u origin main
 
 echo === ✅ Deployment Complete ===
